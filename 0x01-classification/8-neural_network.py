5343be0a66a2337443f57dea7b0728968df85f32
walid-jouad <78660781+walid-jouad@users.noreply.github.com>
2021-03-26 23:31:22 +0100

    Create 8-neural_network.py

---
+++ 0x01-classification/8-neural_network.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class NeuralNetwork:
+ 
+
+    def __init__(self, nx, nodes):
+       
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        # hidden layer
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        # output neuron
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
