6b41341fbdacda3747536025fde90c65b1a790b1
walid-jouad <78660781+walid-jouad@users.noreply.github.com>
2021-03-26 23:22:51 +0100

    Create 5-neuron.py

---
+++ 0x01-classification/5-neuron.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+""" NeuronClass """
+import numpy as np
+
+
+class Neuron:
+   """ NeuronClass """
+
+    def __init__(self, nx):
+     """ NeuronClass initializer  """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+       
+        preactivation = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-preactivation))
+        return self.__A
+
+    def cost(self, Y, A):
+       
+        cost = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = np.sum(cost)
+        cost = - cost / A.shape[1]
+        return cost
+
+    def evaluate(self, X, Y):
+      
+        self.forward_prop(X)
+        prediction = np.where(self.__A >= 0.5, 1, 0)
+        cost = self.cost(Y, self.__A)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+      
+        dz = A - Y
+        dw = np.matmul(X, dz.T) / A.shape[1]
+        db = np.sum(dz) / A.shape[1]
+        self.__W = self.__W - alpha * dw.T
+        self.__b = self.__b - alpha * db
+
+    @property
+    def W(self):
+     """getter for W attribute"""
+        return self.__W
+
+    @property
+    def b(self):
+ 
+        """getter for b attribute"""
+        return self.__b
+
+    @property
+    def A(self):
+     """getter for A attribute"""
+        return self.__A
