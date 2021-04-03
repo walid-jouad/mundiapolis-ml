f712ca862f77a33a3e6d3a300e2ee030b5919fe7
walid-jouad <78660781+walid-jouad@users.noreply.github.com>
2021-04-03 23:04:27 +0200

    Update 12-neural_network.py

--- 0x01-classification/12-neural_network.py
+++ 0x01-classification/12-neural_network.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
-
 import numpy as np
 
 
 class NeuralNetwork:
-  
 
     def __init__(self, nx, nodes):
-  
+
         if type(nx) is not int:
             raise TypeError("nx must be an integer")
         if nx < 1:
@@ -17,36 +15,14 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
-        # hidden layer
+        
         self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
-        # output neuron
+        
         self.__W2 = np.random.randn(1, nodes)
         self.__b2 = 0
         self.__A2 = 0
-
-    def forward_prop(self, X):
-
-        z1 = np.matmul(self.__W1, X) + self.__b1
-        self.__A1 = 1 / (1 + np.exp(-z1))
-        z2 = np.matmul(self.__W2, self.__A1) + self.__b2
-        self.__A2 = 1 / (1 + np.exp(-z2))
-        return self.__A1, self.__A2
-
-    def cost(self, Y, A):
-      
-        cost = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
-        cost = np.sum(cost)
-        cost = - cost / A.shape[1]
-        return cost
-
-    def evaluate(self, X, Y):
-      
-        self.forward_prop(X)
-        prediction = np.where(self.__A2 >= 0.5, 1, 0)
-        cost = self.cost(Y, self.__A2)
-        return prediction, cost
 
     @property
     def W1(self):
@@ -71,3 +47,21 @@ class NeuralNetwork:
     @property
     def A2(self):
         return self.__A2
+    def forward_prop(self, X):
+        z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-z1))
+        z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-z2))
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        cost = Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        cost = np.sum(cost)
+        cost = - cost / A.shape[1]
+        return cost
+
+    def evaluate(self, X, Y):
+        self.forward_prop(X)
+        prediction = np.where(self.__A2 >= 0.5, 1, 0)
+        cost = self.cost(Y, self.__A2)
+        return prediction, cost
